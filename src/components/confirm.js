503d84bb4657525498158c1fdd7e0a1db7f2b873
Bebe-dev <praiseisinkaye@gmail.com>
2023-10-18 21:55:41 +0100

    updating file

--- src/components/confirm.js
+++ src/components/confirm.js
@@ -9,12 +9,14 @@ const Confirm = () => {
   
   const navigate = useNavigate()
   
+  const emailValue = localStorage.getItem("email")
+  
   return(
     <div className="flex flex-col px-6 pt-24 gap-56 md:w-2/5 md:gap-8 md:m-auto">
       <div>
         <img alt="icon" src={iconList} className="icon w-16 p-1" />
         <h1 className="text-darkGrey text-4xl font-bold py-4">Thanks for subscribing!</h1>
-        <p>A confirmation email has been sent to <b>ash@loremcompany.com</b>. Please open it and click the button inside to confirm your subscription.</p>
+        <p>A confirmation email has been sent to <b>{emailValue}</b>. Please open it and click the button inside to confirm your subscription.</p>
       </div>
       <div>
         <button className="border bg-darkGrey py-3 text-white rounded-lg w-full hover:bg-tomato" onClick={() => navigate(-1)} >Dismiss message</button>
--- src/components/form.js
+++ src/components/form.js
@@ -9,10 +9,11 @@ const Form = () => {
   const [isTrue, setIsTrue] = useState(true)
   const handleChange = (e) => {
     setInputValue(e.target.value)
+    setIsTrue(true)
   }
   
   const navigate = useNavigate()
-  
+  localStorage.setItem("email", inputValue)
   
   
   const validateForm = (e) => {
@@ -29,15 +30,13 @@ const Form = () => {
     
   }
   
-  
   const errorMessage = "Valid email required"
-  //{isTrue?"border border-grey pl-4 py-3 rounded-lg mb-5": "border border-tomato pl-4 py-3 rounded-lg mb-5"}
   
   return(
     
     <form className="p-4 flex flex-col">
       <name className="text-darkGrey font-bold flex justify-between" value="address"><small>Email address</small><small className={isTrue?"right":"error"}>{errorMessage}</small></name>
-      <input className={`border pl-4 py-3 rounded-lg mb-5 ${isTrue?"border-grey":"border-tomato text-tomato"}`} placeholder="email@company.com" value={inputValue} onChange={handleChange}/>
+      <input className={`border pl-4 py-3 rounded-lg mb-5 ${isTrue?"border-grey text-primaryColor":"border-tomato text-tomato"}`} placeholder="email@company.com" value={inputValue} onChange={handleChange}/>
       
       <button className="border bg-darkGrey py-3 text-white rounded-lg hover:bg-tomato" onClick={validateForm}>Subscribe to monthly newsletter</button>
     </form>
