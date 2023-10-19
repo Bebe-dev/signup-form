f05d53e29cf1be072bc18d36f654d9a1e31b3e37
bebe-dev <praiseisinkaye@gmail.com>
2023-10-19 11:19:26 +0100

    updating image

--- src/components/backgroundImage.js
+++ src/components/backgroundImage.js
@@ -4,7 +4,7 @@ import '../App.css';
 
 const BackgroundImage = () => {
   return(
-    <div className="bg-mobile bg-center bg-auto bg-no-repeat rounded-xl w-full h-64 min-h-full md:bg-desktop md:bg-contain md:w-1/2 md:h-auto"></div>
+    <div className="bg-mobile bg-center bg-auto bg-no-repeat rounded-xl w-full h-64 min-h-full md:bg-desktop md:bg-auto md:w-1/2 md:h-auto"></div>
   )
 }
 
